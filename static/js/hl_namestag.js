6efe8c019df717f5d22a47b5b1074e4fd8ab4648
Harfenlabor <92948126+Harfenlabor@users.noreply.github.com>
2022-02-25 15:47:02 +0100

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -166,7 +166,7 @@ function populateWithResults(myResults){
 
     //if (myResults[i].charAt[0])
     if (i % 10) {
-      console.log(myResults[i].name.innerText());
+      console.log(myResults[i].name.innerText);
     }
 
 
