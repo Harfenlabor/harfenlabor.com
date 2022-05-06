0173e16884fc8ee8c0b6d31986ffb3f57f0d8e96
Harfenlabor <92948126+Harfenlabor@users.noreply.github.com>
2022-05-06 12:19:35 +0200

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -127,7 +127,7 @@ function persontags(data){
       sortedSubjects.splice(i, 0, { 'name': sortedSubjects[i].subject.charAt(0), 'link': ' ', 'title': sortedSubjects[i].subject.charAt(0)});
     }*/
     if (i > 0) {
-      console.log(sortedSubjects[i-1].subject);
+      console.log(sortedSubjects[i-1].subject.charAt[0]);
     }
 
     //adjust, remove duplicates
